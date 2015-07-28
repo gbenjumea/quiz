549b3f15b5c42649224b30ee15abc7c5a15678a3
gbenjumea <gbenjumea@gmail.com>
2015-07-28 17:12:09 +0200

    multiples preguntas 2 err corregido

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res) {
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
-router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)',quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 /* GET author page*/
