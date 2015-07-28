5d7a5dad517ad8820401270fe62db0defadfa0d3
gbenjumea <gbenjumea@gmail.com>
2015-07-28 10:02:10 +0200

    multiples preguntas

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,10 @@ router.get('/', function(req, res) {
 });
 
 
-
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 /* GET author page*/
 router.get('/author', function(req, res) {
