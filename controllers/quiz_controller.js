995b8fb79a102211f792ba77fabef04897d7dfed
gbenjumea <gbenjumea@gmail.com>
2015-08-03 20:37:13 +0200

    borrar

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -101,3 +101,10 @@ exports.update = function(req, res) {
     }
   }).catch(function(error){next(error)});
 };
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+  req.quiz.destroy().then( function () {
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
+};
