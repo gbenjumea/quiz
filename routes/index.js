e4f00a7d2b40aa9881cd93950d522a645c6a5787
gbenjumea <gbenjumea@gmail.com>
2015-08-10 07:28:35 +0200

    moderacion comentarios

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con ids
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 
 // Definicion de ruta de sesion
 router.get('/login', sessionController.new);     // formmualario login
@@ -38,5 +39,8 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+	                            sessionController.loginRequired, 
+	                            commentController.publish);
 
 module.exports = router;
